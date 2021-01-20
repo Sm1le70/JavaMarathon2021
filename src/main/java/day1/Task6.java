c8aa10fd8f3d355ad08814c0577ed7a659478afc
Sm1le70 <smile99070@gmail.com>
2021-01-20 19:52:28 +0700

    complete day1 tasks

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,8 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        for (int k = 7, i = 1; i <= 9; i++) {
+            System.out.println(i + " x " + k + " = " + i*k);
+        }
     }
 }
