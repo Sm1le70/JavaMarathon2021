0106f5b81978f5fcfece99e02b654350e264051c
Sm1le70 <smile99070@gmail.com>
2021-01-21 21:03:39 +0700

    complete day2 tasks

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,20 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
 
+        if (a >= b) System.out.println("Некорректный ввод");
+        else {
+            ++a;
+            while (a <= b) {
+                if ((a % 5 == 0) && (a % 10 != 0)) System.out.print(a + " ");
+                a++;
+            }
+        }
     }
 }
