834abcf01d0a8bb1cc876b3e6b0be0100411e59e
Sm1le70 <smile99070@gmail.com>
2021-01-22 23:01:30 +0700

    complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,18 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double x = s.nextDouble();
+        double y = s.nextDouble();
 
+        while (y != 0) {
+            System.out.println(x / y);
+
+            x = s.nextDouble();
+            y = s.nextDouble();
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,22 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
+        for (int i = 0; i < 5; i++) {
+            double x = s.nextDouble();
+            double y = s.nextDouble();
+            switch ((int) y) {
+                case 0:
+                    System.out.println("Деление на 0");
+                    break;
+                default:
+                    System.out.println(x / y);
+            }
+
+        }
     }
 }
