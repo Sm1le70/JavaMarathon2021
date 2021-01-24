895a7c2193f47bc7fdf13a652d7448b076082b3d
Sm1le70 <smile99070@gmail.com>
2021-01-24 13:20:44 +0700

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,17 @@
 package day5;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Car car = new Car();
 
+        car.setYear(s.nextInt());
+        s.nextLine(); // поглощает перенос строки после nextInt(), не знаю как сделать по-другому
+        car.setColor(s.nextLine());
+        car.setModel(s.nextLine());
+
+        System.out.println("Год выпуска: " + car.getYear() + ", цвет: " + car.getColor() + " ,модель: " + car.getModel());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,8 +1,19 @@
 package day5;
 
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
+        int year = s.nextInt();
+        s.nextLine(); // поглощает перенос строки после nextInt(), не знаю как сделать по-другому
+        String color = s.nextLine();
+        String model = s.nextLine();
+
+        Motorbike moto = new Motorbike(year, color, model);
+
+        System.out.println("Год выпуска: " + moto.getYear() + ", цвет: " + moto.getColor() + ", модель: " + moto.getModel());
     }
 }
