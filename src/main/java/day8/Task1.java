0892e5eb643cf6b38397f5a4cc1b21f58b4689ad
Sm1le70 <smile99070@gmail.com>
2021-01-27 20:24:27 +0700

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,21 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
 
+//        String string = "";    // 195 мс
+//        for (int i = 0; i < 20001; i++) {
+//            string += i + " ";
+//        }
+//        System.out.println(string);
+
+        StringBuilder sb = new StringBuilder();  // 20 мс
+        for (int i = 0; i < 20001; i++) {
+            sb.append(i + " ");
+        }
+        System.out.println(sb);
+
+        long finish = System.currentTimeMillis();
+        System.out.println(finish - start);
     }
 }
