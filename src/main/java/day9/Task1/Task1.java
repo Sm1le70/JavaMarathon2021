e0a659ea467d0fbf27f027817b83c41ff4f56ef9
Sm1le70 <smile99070@gmail.com>
2021-01-28 22:40:17 +0700

    complete day9 tasks

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -1,7 +1,16 @@
 package day9.Task1;
 
+import java.util.concurrent.TimeoutException;
+
 public class Task1 {
     public static void main(String[] args) {
+        Teacher teacher = new Teacher("Tom", "Math");
+        Student student = new Student("Bob", "12v5");
 
+        System.out.println(teacher.getSubjectName());
+        System.out.println(student.getGroupName());
+
+        teacher.printInfo();
+        student.printInfo();
     }
 }
